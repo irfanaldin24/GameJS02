b5885e70b7dfc318ff0832a240b5471d90dd89db
FanArtes <54843072+FanArtes@users.noreply.github.com>
2020-01-02 05:56:36 +0700

    Add files via upload

---
+++ GoGoFutsall/js/assets.js
@@ -0,0 +1,14 @@
+var canvasBgImage = new Image(),
+    bgPattern;
+canvasBgImage.onload = function () {
+  this.loaded = true;
+  bgPattern = ctx.createPattern(canvasBgImage, 'repeat');
+};
+canvasBgImage.src = "assets/lapangan.jpg";
+
+var ballBgImage = new Image();
+ballBgImage.loaded = false; // custom flag
+ballBgImage.onload = function () {
+  this.loaded = true;
+};
+ballBgImage.src = "assets/bola.png";
---
+++ GoGoFutsall/js/canvas.js
@@ -0,0 +1,6 @@
+// Create the canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 1139;
+canvas.height = 641;
+document.body.appendChild(canvas);
